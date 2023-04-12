3d5dff14f4b6639eb9fbfe66021685c4b6159b00
Joachim <joachiom@stud.ntnu.no>
2023-04-12 18:41:22 +0200

    alt to otsus method

--- MISS/otsus.py
+++ MISS/otsus.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 
-'''
-A function for finding the ideal threshold of an image using otsu's algorithm
-    Parameters:
-        - img - the image of which to find the threshold
-        - bins  - the amount of bins for the histogram (more = more accurate)  
-'''
 def otsus(img, bins):
+    """
+    A function for finding the ideal threshold of an image using otsu's algorithm
+        Parameters:
+            - img - the image of which to find the threshold
+            - bins  - the amount of bins for the histogram (more = more accurate)
+    """
+
     # Get the image histogram and the bin edges(Would implement own histogram function, but it is hard to get bin edges in that case).
     histogram, edges = np.histogram(img, bins=bins)
 
@@ -32,4 +33,23 @@ def otsus(img, bins):
     thresh = centres[:-1][index_of_max_val]
 
     return thresh
+
+
+def otsu_threshold(img):
+    # Compute histogram of pixel intensities
+    hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+    # Normalize histogram
+    hist = hist.astype(float) / hist.sum()
+    # Compute cumulative sums of normalized histogram
+    omega = np.cumsum(hist)
+    # Compute cumulative means of normalized histogram
+    mu = np.cumsum(hist * np.arange(256))
+    # Compute global mean of image
+    global_mu = mu[-1]
+    # Compute between-class variance
+    sigma_b_squared = (global_mu * omega - mu) ** 2 / (omega * (1 - omega))
+    # Find threshold that maximizes between-class variance
+    idx = np.argmax(sigma_b_squared)
+    threshold = bins[idx]
+    return threshold
 
