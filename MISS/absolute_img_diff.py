22940d2f1c5960ba6c77c17ac53cd5dd1d87ef3c
Joachim <joachiom@stud.ntnu.no>
2023-04-14 01:23:52 +0200

    Comments and clarity.

--- MISS/absolute_img_diff.py
+++ MISS/absolute_img_diff.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 
 
 def absolute_img_diff(img1, img2):
-    return np.ndarray.sum(np.abs(img1 - img2)) / (img1.shape[0] * img1.shape[1] * img1.shape[2] * 255)
+    # Calculate the absolute difference between the two images
+    diff = np.ndarray.sum(np.abs(img1 - img2))
+
+    # Normalize to get a value between 0 and 1
+    norm_diff = diff / (img1.shape[0] * img1.shape[1] * img1.shape[2] * 255)
+
+    return round(norm_diff, 4)
 
 
-
-
-#def histogram_comparison(img1, img2):
+# def histogram_comparison(img1, img2):
 #    """method for comparing the histogram for the two images"""
 #    org_hist, edge1 = np.histogram(img1, bins=np.arange(255))
 #    shit_hist, edge2 = np.histogram(img2, bins=np.arange(255))
@@ -27,14 +31,13 @@ def absolute_img_diff(img1, img2):
 #    return edge1, edge2
 #
 #
-#def edge_detection(img1, img2):
+# def edge_detection(img1, img2):
 #    return np.ndarray.sum(np.abs(cv2.Canny(img1) - cv2.Canny(img2)))
 #
 #
-#def kernel_comparison(img1, img2):
+# def kernel_comparison(img1, img2):
 #    return np.ndarray.sum(np.abs(cv2.filter2D(img1) - cv2.filter2D(img2)))
 #
-#print("test1")
-#print(histogram_comparison(org, shit))
-#print("test2")
-
+# print("test1")
+# print(histogram_comparison(org, shit))
+# print("test2")
