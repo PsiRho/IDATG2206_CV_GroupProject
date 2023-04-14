cba564a696c1aea6ba21e6bedaa9e225dc1cc1f6
Joachim <joachiom@stud.ntnu.no>
2023-04-14 16:38:42 +0200

    Removed dirt.

--- MISS/absolute_img_diff.py
+++ MISS/absolute_img_diff.py
@@ -9,33 +9,3 @@ def absolute_img_diff(img1, img2):
     norm_diff = diff / (img1.shape[0] * img1.shape[1] * img1.shape[2] * 255)
 
     return round(norm_diff, 4)
-
-
-# def histogram_comparison(img1, img2):
-#    """method for comparing the histogram for the two images"""
-#    org_hist, edge1 = np.histogram(img1, bins=np.arange(255))
-#    shit_hist, edge2 = np.histogram(img2, bins=np.arange(255))
-#
-#
-#
-#    plt.figure()
-#    plt.subplot(2, 1, 1)
-#    plt.plot(org_hist)
-#    plt.subplot(2, 1, 2)
-#    plt.plot(shit_hist)
-#    plt.show()
-#    cv2.compareHist(org_hist, shit_hist, cv2.HISTCMP_CORREL)
-#
-#    return edge1, edge2
-#
-#
-# def edge_detection(img1, img2):
-#    return np.ndarray.sum(np.abs(cv2.Canny(img1) - cv2.Canny(img2)))
-#
-#
-# def kernel_comparison(img1, img2):
-#    return np.ndarray.sum(np.abs(cv2.filter2D(img1) - cv2.filter2D(img2)))
-#
-# print("test1")
-# print(histogram_comparison(org, shit))
-# print("test2")
